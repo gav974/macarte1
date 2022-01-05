f0c57d46c0a43a4f0a8e48a8c0c506be0fca3cf3
gav974ordi2 <eclapier@msn.com>
2022-01-05 11:55:06 +0400

    insertion de containers et modification des diferrends placements

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+          body: SafeArea(
+            child:Row(
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height:500,
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     Container(
+                       color: Colors.yellow,
+                       width: 100,
+                       height:100,
+                     ),
+                     Container(
+                       color: Colors.green,
+                       width: 100,
+                       height:100,
+                     ),
+                   ],
+                  ),
+                ),
+                Container(
+              padding: EdgeInsets.fromLTRB(30, 10, 50, 20),
+                  color: Colors.blue,
+                  width: 100,
+                  height:500,
+                ),
+              ],
+
+            )
+          ),
+
+
+
+
+
+        )
+        );
+
+  }
 }
